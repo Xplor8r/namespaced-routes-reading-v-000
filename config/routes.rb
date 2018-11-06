dd2b28c5a59f1d6a2154d857e4ad1c909e8b1e4f
Michael Pieschel <sixtyhectares@yahoo.com>
2018-11-06 03:47:44 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :posts, only: %i[show index new edit]
   end
 
-  scope '/admin' do
+  namespace :admin do
     resources :stats, only: [:index]
   end
 
